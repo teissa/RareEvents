895ab40a87f7338f6ca915ddab8fe86673a13411
Tahra Eissa <31454033+teissa@users.noreply.github.com>
2021-08-20 08:26:51 -0600

    Add files via upload

---
+++ VarRareBallModel.m
@@ -0,0 +1,26 @@
+function [ Rare_Ball_Synth] = VarRareBallModel( Prare, Pno, rareMax, HT,  trials )
+%creates synthetic data for the rare ball. Prare and Pno are the
+%probability of giving high or low jar respectively while seeing (or not
+%seeing a rare ball), ht are observations,
+%rareMax is the threshold for rare response
+
+  Rare_Ball_Synth=zeros(trials,1);
+   
+     for x=1:trials
+        indR=find(HT{x}==1);
+        indR=length(indR);
+        rn=rand;
+       if indR>=rareMax && rn<=Prare
+            Rare_Ball_Synth(x)=1;
+       elseif indR>=rareMax && rn> Prare
+           Rare_Ball_Synth(x)=-1;
+       elseif indR<rareMax && rn<= Pno
+            Rare_Ball_Synth(x)=1;
+       elseif indR<rareMax && rn> Pno
+            Rare_Ball_Synth(x)=-1;  
+       end
+
+     end
+
+end
+
